3f08c9a1aee74cd62f460c6e4d205458913814b2
davydhong <davydhong@gmail.com>
2018-10-11 17:01:36 -0700

    bench implemented

--- k6/script.js
+++ k6/script.js
@@ -5,17 +5,33 @@ import { sleep, check } from 'k6';
 // import faker from 'faker';
 
 export let options = {
-  vus: 50,
-  rps: 100,
-  duration: '300s'
+  vus: 100,
+  stages: [
+    {
+      rps: 1000,
+      duration: '120s'
+    },
+    {
+      rps: 2000,
+      duration: '120s'
+    },
+    {
+      rps: 3000,
+      duration: '120s'
+    },
+    {
+      rps: 4000,
+      duration: '120s'
+    }
+  ]
 };
 
 export default function() {
-  var popular_id = Math.ceil(Math.random() * 500 + 10000001);
+  var popular_id = Math.ceil(Math.random() * 5000 + 10000001);
   var id = Math.ceil(Math.random() * 10000000 + 10000001);
   var popular = Math.random() > 0.2;
 
-  let res = http.get(`http://localhost:3001/api/v1/artists/${popular ? popular_id : id}/albums`);
+  let res = http.get(`http://localhost:3000/api/v1/artists/${popular ? popular_id : id}/albums`);
   check(res, {
     'status was 200': r => r.status == 200,
     'server under load threshold': r => r.status !== 503,
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('newrelic')
+require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -6,32 +6,63 @@ const cors = require('cors');
 var compression = require('compression');
 var server = express();
 
-require('console-stamp')(console, 'HH:MM:ss.l');
+// require('console-stamp')(console, 'HH:MM:ss.l');
 server.use(compression());
-server.use(require('morgan')('short'));
+// server.use(require('morgan')('short'));
 
 server.use(bodyParser.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(express.static(path.join(__dirname, './'), { maxAge: '30 days' }));
+server.use('/', express.static(path.join(__dirname, './'), { maxAge: '30 days' }));
+server.use('/artists/:artistID/', express.static(path.join(__dirname, './'), { maxAge: '30 days' }));
+
 server.use(cors());
 // Albums & Player
-server.get('/artists/:artistID/albums/', (req, res) => {
-  res.redirect('http://52.15.129.193' + req.url);
+
+server.all('/api/v1/artists/:artistID/albums/', (req, res, next) => {
+  // console.log('http://localhost:3001' + req.url);
+  res.redirect('http://ec2-54-164-130-42.compute-1.amazonaws.com' + req.url);
+  next();
 });
 
-// Related Artists
-server.get('/artist/:id/relatedArtists', (req, res) => {
-  res.redirect('http://18.206.245.56' + req.url);
-});
+// // Related Artists
+// server.all('/artist/:id/relatedArtists', (req, res) => {
+//   res.redirect('http://18.206.245.56' + req.url);
+// });
 
-// Popular Songs
-server.get('/artist/:id', (req, res) => {
-  res.redirect('http://18.224.17.253' + req.url);
-});
+// // Popular Songs
+// server.all('/artist/:id', (req, res) => {
+//   res.redirect('http://18.224.17.253' + req.url);
+// });
 
-// Header
-server.get('/artists/:artistID', (req, res) => {
-  res.redirect('http://35.172.133.115' + req.url);
-});
+// // Header
+// server.all('/artists/:artistID', (req, res) => {
+//   res.redirect('http://35.172.133.115' + req.url);
+// });
 
-server.listen(3000, console.log('Listening on:', 3000));
+if (process.env.NODE_MULTI) {
+  const cluster = require('cluster');
+  const numCPUs = require('os').cpus().length;
+
+  if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+
+    server.listen(3000, console.log('Listening on:', 3000));
+
+    console.log(`Worker ${process.pid} started`);
+  }
+} else {
+  console.log('Running on Single Core');
+  server.listen(3000, console.log('Listening on:', 3000));
+}
